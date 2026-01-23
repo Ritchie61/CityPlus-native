a52f990afb9b3596496882da71fd83cccb132cbe
Ritchie61 <124876391+Ritchie61@users.noreply.github.com>
2026-01-23 12:26:49 +1000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // all screens and layout files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
